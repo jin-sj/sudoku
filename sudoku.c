716210c83aa8c9d4befdd4a5defa22ea0525f98b
jin-sj <sj_jin@outlook.com>
2019-02-27 17:13:28 -0800

    add generator that checks for solvability each time, and added documentation

--- sudoku.c
+++ sudoku.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "sudoku.h"
 
 #define TRUE 1
@@ -59,6 +60,10 @@ void create_sample_puzzle(int (*array)[NUM_COLS]) {
     add_permanent(array, 8, 6, 3);
 }
 
+/** Initializes all cells to EMPTY value
+ *
+ * @param array Reference to array
+ */
 void create_empty_puzzle(int (*array)[NUM_COLS]) {
     int r, c;
     for (r = 0; r < NUM_ROWS; ++r) {
@@ -66,9 +71,87 @@ void create_empty_puzzle(int (*array)[NUM_COLS]) {
             array[r][c] = EMPTY;
         }
     }
-    //array = {{EMPTY}};
 }
 
+/** Generates a new radom puzzle
+ *
+ * @param array Reference to array
+ */
+void create_new_puzzle(int (*array)[NUM_COLS]) {
+    int test_array[NUM_ROWS][NUM_COLS];
+
+    create_empty_puzzle(array);
+    create_empty_puzzle(test_array);
+
+    //fewest clues possible for proper sudoku is 17
+    int starting_amount_needed = 19;
+    int numbers_inserted = 0;
+
+    int possible_values[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    while (numbers_inserted < starting_amount_needed) {
+        printf("Added: %d numbers", numbers_inserted);
+        //get random unassigned cell
+        int random_row = generate_random_number(0, 8);
+        int random_col = generate_random_number(0, 8);
+
+        while (get_value_in(array, random_row, random_col) != EMPTY) {
+            random_row = generate_random_number(0, 8);
+            random_col = generate_random_number(0, 8);
+        }
+
+        int test_value;
+        int random_possible_value_index = generate_random_number(0, 8);
+
+        int i;
+        int index;
+        for (i = 0; i < 9; ++i) {
+            index = (i + random_possible_value_index) % 8;
+            test_value = possible_values[index];
+            if (is_legal_move(array, random_row, random_col, test_value)) {
+                add_permanent(test_array, random_row, random_col, test_value);
+                if (sudoku_solver(test_array)) {
+                    add_permanent(array, random_row, random_col, test_value);
+                    copy_array(array, test_array);
+                    break;
+                }
+            }
+        }
+        numbers_inserted += 1;
+    }
+    display(array);
+}
+
+/** Copies the values from source to target array
+ *
+ * @param source_array  Source array
+ * @param target_array  Target array
+ */
+void copy_array(int (*source_array)[NUM_COLS], int (*target_array)[NUM_COLS]) {
+    int r, c;
+    reset(target_array);
+    for (r = 0; r < NUM_ROWS; ++r) {
+        for (c = 0; c < NUM_COLS; ++c) {
+            add_permanent(target_array, r, c, get_value_in(source_array, r, c));
+        }
+    }
+}
+
+/** Generates a random int from range
+ *
+ * @param low  Low value
+ * @param high  High value
+ * @return  Random value
+ */
+int generate_random_number(int low, int high) {
+    srand(time(0));
+    return (rand() % (high - low + 1)) + low;
+}
+
+/** Prints a given array
+ *
+ * @param array  Array
+ */
 void display(int (*array)[NUM_COLS]) {
     int r;
     int c;
@@ -84,6 +167,14 @@ void display(int (*array)[NUM_COLS]) {
     }
 }
 
+/** Sets the value of a cell
+ *
+ * @param array  Array to update
+ * @param row  Row index
+ * @param col  Column index
+ * @param value  Value to add
+ * @param is_permanent  Whether it is a permanent value
+ */
 void set_cell_value(int (*array)[NUM_COLS], int row, int col, int value, int is_permanent) {
     if (value >= 0 && value < 10) {
         if (is_permanent) {
@@ -94,29 +185,60 @@ void set_cell_value(int (*array)[NUM_COLS], int row, int col, int value, int is_
     }
 }
 
+/** Adds the user's guess
+ *
+ * @param array  Array to update
+ * @param row  Row index
+ * @param col  Column index
+ * @param value  Value to add
+ */
 void add_guess(int (*array)[NUM_COLS], int row, int col, int value) {
     set_cell_value(array, row, col, value, FALSE);
 }
 
+/** Adds permanent values to the puzzle
+ *
+ * @param array  Array to update
+ * @param row  Row index
+ * @param col  Column index
+ * @param value  Value to add
+ */
 void add_permanent(int (*array)[NUM_COLS], int row, int col, int value) {
     set_cell_value(array, row, col, value, TRUE);
 }
 
+/** Resets a cell's value to empty value
+ *
+ * @param array  Array to update
+ * @param row  Row index
+ * @param col  Column index
+ */
 void reset_cell(int (*array)[NUM_COLS], int row, int col) {
     set_cell_value(array, row, col, EMPTY, FALSE);
 }
 
+/** Gets the current value in a cell
+ *
+ * @param array  Array to update
+ * @param row  Row index
+ * @param col  Column index
+ */
 int get_value_in(int (*array)[NUM_COLS], int row, int col) {
     return abs(array[row][col]);
 }
 
+/** Solves the sudoku with the back-tracking algorithm
+ *
+ * @param array  Puzzle to solve
+ * @return  TRUE if solved, else FALSE
+ */
 int sudoku_solver(int (*array)[NUM_COLS]) {
     int r, c;
     int test_value;
     for (r = 0; r < NUM_ROWS; ++r) {
         for (c = 0; c < NUM_COLS; ++c) {
             if (array[r][c] == EMPTY) {
-                for (test_value = MIN_VALUE; test_value < MAX_VALUE; ++test_value) {
+                for (test_value = MIN_VALUE; test_value <= MAX_VALUE; ++test_value) {
                     if (is_legal_move(array, r, c, test_value)) {
                         add_guess(array, r, c, test_value);
                         if (sudoku_solver(array) == TRUE) {
@@ -126,17 +248,20 @@ int sudoku_solver(int (*array)[NUM_COLS]) {
                         }
                     }
                 }
-                printf("Not solvable\n");
-                display(array);
                 return FALSE;
             }
         }
     }
-    printf("SOLVED!\n");
-    display(array);
     return TRUE;
 }
 
+/** Checks if the given value to add is legal
+ *
+ * @param array  Array to update
+ * @param row  Row index
+ * @param col  Column index
+ * @param value  Value to check
+ */
 int is_legal_move(int (*array)[NUM_COLS], int row, int col, int value) {
     if (!is_permanent_value(array, row, col) &&
         !value_in_row(array, row, value) &&
@@ -148,6 +273,13 @@ int is_legal_move(int (*array)[NUM_COLS], int row, int col, int value) {
     }
 }
 
+/** Checks if the cell is a permanent cell
+ *
+ * @param array  Array to check
+ * @param row  Row index
+ * @param col Column index
+ * @return Whether it is a permanent cell
+ */
 int is_permanent_value(int (*array)[NUM_COLS], int row, int col) {
     if (get_value_in(array, row, col) < 0) {
         return TRUE;
@@ -156,6 +288,12 @@ int is_permanent_value(int (*array)[NUM_COLS], int row, int col) {
     }
 }
 
+/** Checks if the value is in a given row
+ *
+ * @param array  Array to check
+ * @param row  Row index
+ * @return Whether value is in row
+ */
 int value_in_row(int (*array)[NUM_COLS], int row, int value) {
     int c;
     for (c = 0; c < NUM_COLS; ++c) {
@@ -166,6 +304,12 @@ int value_in_row(int (*array)[NUM_COLS], int row, int value) {
     return FALSE;
 }
 
+/** Checks if the value is in a given col
+ *
+ * @param array  Array to check
+ * @param col Column index
+ * @return Whether value is in column
+ */
 int value_in_col(int (*array)[NUM_COLS], int col, int value) {
     int r;
     for (r = 0; r < NUM_ROWS; ++r) {
@@ -176,6 +320,13 @@ int value_in_col(int (*array)[NUM_COLS], int col, int value) {
     return FALSE;
 }
 
+/** Checks if the value is in a given col
+ *
+ * @param array  Array to check
+ * @param row Row index
+ * @param col Column index
+ * @return Whether value is in the associated 9x9 square
+ */
 int value_in_square(int (*array)[NUM_COLS], int row, int col, int value) {
     int r, c;
     int r_start, r_end;
@@ -234,7 +385,6 @@ int is_full(int (*array)[NUM_COLS], int row, int col) {
 }
 
 /** Resets non-permanent values to 0
- *
  */
 void reset(int (*array)[NUM_COLS]) {
     int r, c;
@@ -248,23 +398,15 @@ void reset(int (*array)[NUM_COLS]) {
     }
 }
 
-int test(int (*array)[NUM_COLS]) {
-    if (sudoku_solver(array)) {
-        printf("SOLVER is probably working\n");
-        return TRUE;
-    } else {
-        printf("SOLVER is NOT working\n");
-        return FALSE;
-    }
-}
-
 int main(void) {
+    setbuf(stdout, NULL);
     int board[NUM_ROWS][NUM_COLS];
     int sample_board[NUM_ROWS][NUM_COLS];
-    create_empty_puzzle(sample_board);
-    create_sample_puzzle(sample_board);
-    display(sample_board);
-    //printf("value in row: %d\n", value_in_row(sample_board, 0, 4));
-    sudoku_solver(sample_board);
+    //create_empty_puzzle(sample_board);
+    //create_sample_puzzle(sample_board);
+    create_empty_puzzle(board);
+    printf("generating new board\n");
+    create_new_puzzle(board);
+    display(board);
     return 1;
 }
--- sudoku.h
+++ sudoku.h
@@ -7,9 +7,10 @@
 
 static const int NUM_COLS = 9;
 
-void create_sample_puzzle(int (*array)[NUM_COLS]);
-
 void create_empty_puzzle(int (*array)[NUM_COLS]);
+void create_sample_puzzle(int (*array)[NUM_COLS]);
+void create_new_puzzle(int (*array)[NUM_COLS]);
+
 void display(int (*array)[NUM_COLS]);
 
 void set_cell_value(int (*array)[NUM_COLS], int row, int col, int value, int is_permanent);
@@ -17,9 +18,12 @@ void add_guess(int (*array)[NUM_COLS], int row, int col, int value);
 void add_permanent(int (*array)[NUM_COLS], int row, int col, int value);
 int get_value_in(int (*array)[NUM_COLS], int row, int col);
 
+void copy_array(int (*source_array)[NUM_COLS], int (*target_array)[NUM_COLS]);
 void reset_cell( int (*array)[NUM_COLS], int row, int col);
 
 int sudoku_solver(int (*array)[NUM_COLS]);
+
+int generate_random_number(int low, int high);
 
 int is_legal_move(int (*array)[NUM_COLS] , int row, int col, int value);
 int is_permanent_value(int (*array)[NUM_COLS], int row, int col);
@@ -29,6 +33,4 @@ int value_in_square(int (*array)[NUM_COLS], int row, int col, int value);
 
 int is_full(int (*array)[NUM_COLS], int row, int col);
 void reset(int (*array)[NUM_COLS]);
-
-int test(int (*array)[NUM_COLS]);
 #endif //SUDOKU_SUDOKU_H
